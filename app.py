e862c6f64f7c737ba356a7dfe64166bc8b440279
hasnain <hasnainkhankqs@gmail.com>
2021-02-06 14:07:26 +0500

    pushing with problem font familty

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask , render_template
-
-
+from flask import Flask , render_template , redirect , url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html" , active='index')
+
+
+@app.route("/home")
+def home():
+    return render_template("index.html" , active='index')
+
+@app.route("/about")
+def about():
+    return render_template("about.html" , active='about')
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html" , active='blog')
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html" , active='contact')
 
 
 
